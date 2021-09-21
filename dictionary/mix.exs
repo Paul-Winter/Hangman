735f734869e9181b01293e33d30ba9c484283624
ZiminPV <zimin.pavel.v@gmail.com>
2021-09-21 17:08:52 +0300

    added application

--- dictionary/mix.exs
+++ dictionary/mix.exs
@@ -3,9 +3,9 @@ defmodule Dictionary.MixProject do
 
   def project do
     [
-      app: :dictionary,
+      app:     :dictionary,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir:  "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -14,7 +14,8 @@ defmodule Dictionary.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      mod: { Dictionary.Application, [] },
+      extra_applications: [:logger],
     ]
   end
 
