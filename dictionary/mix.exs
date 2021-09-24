74ff0a3a1c3422c923406fa0d6f7f993a90b82c8
ZiminPV <zimin.pavel.v@gmail.com>
2021-09-24 11:48:58 +0300

    some changes

--- dictionary/mix.exs
+++ dictionary/mix.exs
@@ -3,7 +3,7 @@ defmodule Dictionary.MixProject do
 
   def project do
     [
-      app:     :hangman,
+      app:     :dictionary,
       version: "0.1.0",
       elixir:  "~> 1.12",
       build_embedded: Mix.env == :prod,
@@ -15,17 +15,14 @@ defmodule Dictionary.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: { Hangman.Application, [] },
-      extra_applications: [
-        :logger
-      ]
+      mod: { Dictionary.Application, [] },
+      extra_applications: [:logger],
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      { :dictionary, path: "../dictionary" },
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
