5720b6f7ab00f6d7c2b2cc5155fb4f3049e6d20a
ZiminPV <zimin.pavel.v@gmail.com>
2021-10-05 10:34:28 +0300

    added status html file

--- gallows/lib/gallows_web/views/helpers/game_state_helper.ex
+++ gallows/lib/gallows_web/views/helpers/game_state_helper.ex
@@ -3,10 +3,10 @@ defmodule GallowsWeb.Views.Helpers.GameStateHelper do
   import Phoenix.HTML, only: [ raw: 1 ]
 
   @responses %{
-    :won          => { :success, "You Won!" },
-    :lost         => { :danger,  "You Lost!" },
-    :good_guess   => { :success, "Good guess!" },
-    :bad_guess    => { :warning, "Bad guess!" },
+    :won          => { :success, "You Won!"                 },
+    :lost         => { :danger,  "You Lost!"                },
+    :good_guess   => { :success, "Good guess!"              },
+    :bad_guess    => { :warning, "Bad guess!"               },
     :already_used => { :info,    "You already guessed that" },
   }
 
