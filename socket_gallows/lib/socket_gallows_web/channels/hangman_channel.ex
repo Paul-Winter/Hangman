446dc786becc6e762453463e0de6b920d66ed537
ZiminPV <zimin.pavel.v@gmail.com>
2021-10-05 16:46:45 +0300

    added join to hangman channel

--- socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
+++ socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
@@ -1,32 +1,9 @@
 defmodule SocketGallowsWeb.HangmanChannel do
-  use SocketGallowsWeb, :channel
 
-  @impl true
-  def join("hangman:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+  use Phoenix.Channel
+
+  def join("hangman:game", _, socket) do
+    { :ok, socket }
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (hangman:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
-    {:noreply, socket}
-  end
-
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
-  end
 end
