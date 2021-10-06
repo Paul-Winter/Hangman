96e25ae83fcfa651d00766d4bd2340ad59fc8705
ZiminPV <zimin.pavel.v@gmail.com>
2021-10-06 16:30:01 +0300

    added tally to server

--- socket_gallows/mix.exs
+++ socket_gallows/mix.exs
@@ -45,7 +45,8 @@ defmodule SocketGallows.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:hangman, [ path: "../hangman" ]}
     ]
   end
 
