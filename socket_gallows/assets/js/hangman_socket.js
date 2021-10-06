4fc5139fdc03f7cec7ac8367c84b5f0ef605ca47
ZiminPV <zimin.pavel.v@gmail.com>
2021-10-06 13:57:09 +0300

    added hangman_socket and hangman_app js

---
+++ socket_gallows/assets/js/hangman_socket.js
@@ -0,0 +1,22 @@
+import { Socket } from "phoenix"
+
+export default class HangmanSocket {
+
+    constructor() {
+        this.socket = new Socket("/socket", {})
+        this.socket.connect()
+    }
+
+    connect_to_hangman() {
+        this.channel = this.socket.channel("hangman:game", {})
+        this.channel
+            .join()
+            .receive("ok", resp => {
+                console.log("connected: " + resp)
+            })
+            .receive("error", resp => {
+                alert(resp)
+                throw(resp)
+            })
+    }
+}
