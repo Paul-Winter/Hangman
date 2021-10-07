8f9b4cc897c127634385080b0747292bd4f0c893
ZiminPV <zimin.pavel.v@gmail.com>
2021-10-07 12:10:22 +0300

    changed config files

--- gallows/config/config.exs
+++ gallows/config/config.exs
@@ -1,48 +1,23 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
+# and its dependencies with the aid of the Mix.Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
-
-# General application configuration
-import Config
+use Mix.Config
 
 # Configures the endpoint
-config :gallows, GallowsWeb.Endpoint,
+config :gallows, Gallows.Web.Endpoint,
   url: [host: "localhost"],
-  render_errors: [view: GallowsWeb.ErrorView, accepts: ~w(html json), layout: false],
-  pubsub_server: Gallows.PubSub,
-  live_view: [signing_salt: "goFFmpWM"]
-
-# Configures the mailer
-#
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :gallows, Gallows.Mailer, adapter: Swoosh.Adapters.Local
-
-# Swoosh API client is needed for adapters other than SMTP.
-config :swoosh, :api_client, false
-
-# Configure esbuild (the version is required)
-config :esbuild,
-  version: "0.12.18",
-  default: [
-    args: ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets),
-    cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-  ]
+  secret_key_base: "95U3rjIaf3PUiJXsztlYJmaN2bLgv4FSCxYZCqLq/jhzBhzh4xBGOkxaZ1E3JMAg",
+  render_errors: [view: Gallows.Web.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Gallows.PubSub,
+           adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{config_env()}.exs"
+import_config "#{Mix.env}.exs"
