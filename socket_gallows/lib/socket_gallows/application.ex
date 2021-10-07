32a93ea353ea099d7b04d64a0131e6a2609e7108
ZiminPV <zimin.pavel.v@gmail.com>
2021-10-07 16:09:43 +0300

    changed web files

--- socket_gallows/lib/socket_gallows/application.ex
+++ socket_gallows/lib/socket_gallows/application.ex
@@ -1,34 +1,22 @@
 defmodule SocketGallows.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
-  @impl true
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
   def start(_type, _args) do
+    import Supervisor.Spec
+
+    # Define workers and child supervisors to be supervised
     children = [
-      # Start the Telemetry supervisor
-      SocketGallowsWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: SocketGallows.PubSub},
-      # Start the Endpoint (http/https)
-      SocketGallowsWeb.Endpoint
-      # Start a worker by calling: SocketGallows.Worker.start_link(arg)
-      # {SocketGallows.Worker, arg}
+      # Start the endpoint when the application starts
+      supervisor(SocketGallows.Web.Endpoint, []),
+      # Start your own worker by calling: SocketGallows.Worker.start_link(arg1, arg2, arg3)
+      # worker(SocketGallows.Worker, [arg1, arg2, arg3]),
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SocketGallows.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
-  @impl true
-  def config_change(changed, _new, removed) do
-    SocketGallowsWeb.Endpoint.config_change(changed, removed)
-    :ok
   end
 end
--- socket_gallows/lib/socket_gallows/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule SocketGallows.Mailer do
-  use Swoosh.Mailer, otp_app: :socket_gallows
-end
