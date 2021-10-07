07e5c9b3b2c0a3779f68b72847b88982a5b2d150
ZiminPV <zimin.pavel.v@gmail.com>
2021-10-07 11:54:23 +0300

    deleted build_statement from mix.project

--- dictionary/mix.exs
+++ dictionary/mix.exs
@@ -6,8 +6,7 @@ defmodule Dictionary.MixProject do
       app:     :dictionary,
       version: "0.1.0",
       elixir:  "~> 1.12",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env() == :prod,
+      start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
