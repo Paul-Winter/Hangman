19b3b5c38cddf873293e133070724ecc108394bd
ZiminPV <zimin.pavel.v@gmail.com>
2021-10-07 16:22:05 +0300

    changed channels and controller files

--- socket_gallows/lib/socket_gallows/web/channels/hangman_channel.ex
+++ socket_gallows/lib/socket_gallows/web/channels/hangman_channel.ex
@@ -1,4 +1,4 @@
-defmodule SocketGallowsWeb.HangmanChannel do
+defmodule SocketGallows.Web.HangmanChannel do
 
   use Phoenix.Channel
 
@@ -6,7 +6,7 @@ defmodule SocketGallowsWeb.HangmanChannel do
 
   def join("hangman:game", _, socket) do
     socket = socket |> assign(:game, Hangman.new_game())
-    { :ok, socket }
+    {:ok, socket }
   end
 
   def handle_in("tally", _, socket) do
@@ -15,7 +15,7 @@ defmodule SocketGallowsWeb.HangmanChannel do
     { :noreply, socket }
   end
 
-  def handle_in("make_move", _, socket) do
+  def handle_in("make_move", guess, socket) do
     tally = socket.assigns.game |> Hangman.make_move(guess)
     push(socket, "tally", tally)
     { :noreply, socket }
--- socket_gallows/lib/socket_gallows/web/controllers/page_controller.ex
+++ socket_gallows/lib/socket_gallows/web/controllers/page_controller.ex
@@ -1,7 +1,7 @@
-defmodule SocketGallowsWeb.PageController do
-  use SocketGallowsWeb, :controller
+defmodule SocketGallows.Web.PageController do
+  use SocketGallows.Web, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render conn, "index.html"
   end
 end
